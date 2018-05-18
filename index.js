690cb61d38df626248849299f8bba7c4714a1c95
Colin Zhang <l8v0b@ugrad.cs.ubc.ca>
2018-05-18 14:28:02 -0700

    First step

---
+++ index.js
@@ -0,0 +1,6 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+pppejefeifiaoed;sdsn'w'w'w'w''
+bot.on();
+
+bot.login(NDQ2NDgyNDQwMzQ5OTQxODAx.Dd9cuA.YedkUB3574jTEbLauBxA9OcjpSE);
